953899788bab8d1cf3aa9001e1c5c417b10912fb
gflores150 <floreslopezgabriel@outlook.com>
2026-01-29 11:23:34 -0400

    Add project files.

---
+++ how far i can go with this/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace how_far_i_can_go_with_this
+{
+    internal class Model
+    {
+        Dictionary<int, string> id = new()
+        {
+            [1] = "15134651351",
+            [2] = "561356546",
+            [3] = "456546464564"
+        };
+        public List<string> user = new()
+            {
+                "Alice", "Alice@gmail.com",
+                "Bob", "Bob@icloud.com",
+                "Charlie", "Charlie@outlook.com"
+            };
+        public void Names()
+        {
+            foreach (var number in id)
+            {
+                Console.WriteLine($"{number.Key}: {number.Value}");
+            }
+            foreach (var person in user)
+            {
+                Console.WriteLine($"user:{person}");
+            }
+        }
+    }
+}
---
+++ how far i can go with this/Program.cs
@@ -0,0 +1,31 @@
+﻿using how_far_i_can_go_with_this;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Dictionary<int, string> numbers = new()
+        {
+            [1] = "One",
+            [2] = "Two",
+            [3] = "Three"
+        };
+        Console.WriteLine();
+
+        Model mymodel = new Model();
+        mymodel.Names();
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine($"{number.Key}: {number.Value}");
+
+        }
+        Console.WriteLine();
+        Console.WriteLine("user list");
+        foreach (var person in mymodel.user)
+        {
+            Console.WriteLine($"user:{person}");
+        }
+    }
+}
